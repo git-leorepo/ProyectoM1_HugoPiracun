2f62ebf83814a41c9af492ca1c8ffb7e860e522f
HugoP <leogitrepo@gmail.com>
2026-05-11 17:21:26 -0500

    feat[JS]: se agregan las cards con el radio button y se le añade un efecto al aparecer

--- js/script.js
+++ js/script.js
@@ -1,33 +1,75 @@
 /*ELEMENTOS DEL DOM*/
 
-//Elemento Contenedor del DOM
-const divContenedor = document.querySelector('.contenedor')
-//Elemento Agregar Color del DOM
-const btnAgregarItem = document.getElementById('botonAgregarColor')
+//Elemento article class: class-container
+const seccionContenedor = document.querySelector(".contenedor")
+
 //Elemento Generar Paleta en DOM
 const btnGenerarPaleta = document.getElementById('botonGenerarPaleta')
-//Arreglo de colores para paletas 
+
+//Btn Generar Card
+const btnGenerarCard = document.querySelector("#botonGenerarCard")
+
+//Radio Button Colores
+const radioBtnColor = document.querySelector("#contenedor-radio-colores")
 
 
 /*FUNCIONES*/
 
 //Funcion generar aleatorio
-
 function generarAleatorio(){
     let aleatorio_paleta = Math.floor(Math.random()*23);
     return aleatorio_paleta;
 }
 
-//Funcion Añadir Item: Genera un nuevo div con propiedades definidas
-
-
-
-
-
-
-function anadirItem(){
-    //maximo puedo adicionar 9 colores        
+//Funcion Añadir Card: Genera una nueva card basandome en el valor enviado en los radio Buttons
+function anadirCard(){
+    let i = 0;    
+    seccionContenedor.innerHTML = "";
+    //Añado el valor del radio button seleccionado a una variable, si no hay ninguno seleccionado, se asigna un valor por defecto de 6
+    const radioBtnSeleccionado = document.querySelector('input[name="palette-size"]:checked');
+    const valor = radioBtnSeleccionado ? radioBtnSeleccionado.value : 6;
+    do{
+        crearCard()
+        i++;
+        console.log(i)
+    }while(i<parseInt(valor))
+    i=1;
 }
+
+
+function valoresRadio(){
+    console.log(event.target.value)
+}
+
+function crearCard(){
+    //Creo el article que contiene la card    
+        const nuevoArticle = document.createElement("article");
+        nuevoArticle.classList.add("class-container");
+        // creo el div que es la card
+        const nuevoDiv = document.createElement("div");
+        nuevoDiv.classList.add("layer-top")
+        //creo el parrafo que tiene el nombre del color
+        const parrafoColor = document.createElement("p");        
+        parrafoColor.classList.add("texto-color");
+        parrafoColor.textContent = "color"
+
+        seccionContenedor.appendChild(nuevoArticle);
+        nuevoArticle.appendChild(nuevoDiv);
+        nuevoArticle.appendChild(parrafoColor);
+}
+
+radioBtnColor.addEventListener('change', anadirCard)
+
+// Generar cards por defecto cuando carga la página
+anadirCard()
+
+
+
+
+
+
+
+
 
 
 //Funcion cambiar color: Cambia de paleta de colores al darle clic al boton
